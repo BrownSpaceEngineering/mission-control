d62d2f9d8d019640e819658121c0a4d6c2ffdc4b
Shawna Huang <shawna_huang@brown.edu>
2017-11-05 07:26:23 -0500

    css for twitterfeed

--- client/modules/Desktop/Static/TwitterBox.js
+++ client/modules/Desktop/Static/TwitterBox.js
@@ -23,16 +23,14 @@ class TwitterBox extends Component {
   render() {
     return (
       <div className={style.twitterBox}>
+        <h3>Twitter Feed</h3>
         {this.state.tweets &&
           this.state.tweets.map((each) => {
-            console.log(each.user.profile_image_url_https);
             return (
-              <div key={each.id}>
-                <img src={each.user.profile_image_url_https} alt="profile" className="profile_image" />
-                <div className="username"><p>{each.user.name} @{each.user.screen_name}</p></div>
-                {each.text}
-                <br />
-                ---------------
+              <div key={each.id} className={style.tweet}>
+                <img src={each.user.profile_image_url_https} alt="profile" className={style.profile_image} />
+                <div className={style.username}><p>{each.user.name}&nbsp; &nbsp;@{each.user.screen_name}</p></div>
+                <div className={style.text}>{each.text}</div>
               </div>
             );
           })
