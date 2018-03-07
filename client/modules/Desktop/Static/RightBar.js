b6007f4d1489297be771cc50ec8cdbb208c0e684
njeisele <nickolas_eisele@brown.edu>
2018-03-07 18:43:13 -0500

    cesium right bar

--- client/modules/Desktop/Static/RightBar.js
+++ client/modules/Desktop/Static/RightBar.js
@@ -7,18 +7,18 @@ class RightBar extends Component {
   render() {
     return (
       <div className={style.rightBar}>
-      <h2>TEMPERATURE</h2>
-      <h1>XX</h1>
-      <br />
-      <h2>ALTITUDE</h2>
-      <h1>XX</h1>
-      <br />
-      <h2>COORDINATES</h2>
-      <h1>XX</h1>
-      <h1>XX</h1>
-      <div className={style.copyright}>
-      	<p>&copy; BSE 2018 </p>
-      </div>
+        <h2>TEMPERATURE</h2>
+        <h1>XX</h1>
+        <br />
+        <h2>ALTITUDE</h2>
+        <h1>XX</h1>
+        <br />
+        <h2>COORDINATES</h2>
+        <h1>XX</h1>
+        <h1>XX</h1>
+        <div className={style.copyright}>
+          <p>&copy; BSE 2018 </p>
+        </div>
       </div>
     );
   }
