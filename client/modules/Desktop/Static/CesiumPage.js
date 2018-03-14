f07638391a670ba365546a9872f201333fde77d3
Andrew Kim <therladbsgh@gmail.com>
2018-03-14 19:11:52 -0400

    Aesthetic changes to cesium

--- client/modules/Desktop/Static/CesiumPage.js
+++ client/modules/Desktop/Static/CesiumPage.js
@@ -1,68 +1,73 @@
 import React, { Component } from 'react';
 
 import style from './CesiumPage.css';
-import RightBar from "./RightBar.js";
+import RightBar from './RightBar.js';
 const Cesium = global.Cesium;
 
 class CesiumPage extends Component {
   componentDidMount() {
-    var viewer = new Cesium.Viewer('cesiumContainer', {
+    Cesium.BingMapsApi.defaultKey = 'AoB0m6xdYV5QFN7G597_nodN55DfzuUyr-7-' +
+                                    'jjBSUSB1dea5LZPLNIBeIJYfB-59';
+
+    const viewer = new Cesium.Viewer('cesiumContainer', {
+      animation: false,
       shouldAnimate: true,
+      timeline: false,
+      fullscreenButton: false,
     });
 
-    var scene = viewer.scene;
+    // viewer.scene.mode = Cesium.SceneMode.SCENE2D;
 
-    var pathPosition = new Cesium.SampledPositionProperty();
-    var entityPath = viewer.entities.add({
-        position: pathPosition,
-        name: 'path',
-        path: {
-            show: true,
-            leadTime: 0,
-            trailTime: 600000,
-            width: 10,
-            resolution: 1,
-            material: new Cesium.PolylineGlowMaterialProperty({
-                glowPower: 0.3,
-                color: Cesium.Color.PALEGOLDENROD,
-            })
-        }
+    const pathPosition = new Cesium.SampledPositionProperty();
+    viewer.entities.add({
+      position: pathPosition,
+      name: 'path',
+      path: {
+        show: true,
+        leadTime: 0,
+        trailTime: 600000,
+        width: 10,
+        resolution: 1,
+        material: new Cesium.PolylineGlowMaterialProperty({
+          glowPower: 0.3,
+          color: Cesium.Color.PALEGOLDENROD,
+        }),
+      },
     });
 
-    var camera = viewer.camera;
-    var position = Cesium.Cartesian3.fromDegrees(-123.0744619, 44.0503706, 5000.0);
+    const position = Cesium.Cartesian3.fromDegrees(-123.0744619, 44.0503706, 5000.0);
 
-    var entity = viewer.entities.add({
-        name : "AA",
-        position : position,
-        model : {
-            uri : '/cad/Heart.gltf',
-        }
+    const entity = viewer.entities.add({
+      name: 'AA',
+      position,
+      model: {
+        uri: '/cad/Heart.gltf',
+      },
     });
 
     viewer.trackedEntity = entity;
 
-    var clock = viewer.clock;
-    var lastUpdated = clock.currentTime;
+    const clock = viewer.clock;
     clock.onTick.addEventListener(() => {
-        var pos = entity.position._value;
-        var cart = Cesium.Ellipsoid.WGS84.cartesianToCartographic(pos);
-        pos = Cesium.Cartesian3.fromRadians(cart.longitude + 0.000001, cart.latitude, cart.height);
-        pathPosition.addSample(Cesium.JulianDate.now(), pos);
-        entity.position = pos;
+      let pos = entity.position._value;
+      const cart = Cesium.Ellipsoid.WGS84.cartesianToCartographic(pos);
+      pos = Cesium.Cartesian3.fromRadians(cart.longitude + 0.000001, cart.latitude, cart.height);
+      pathPosition.addSample(Cesium.JulianDate.now(), pos);
+      entity.position = pos;
     });
   }
 
   render() {
     return (
       <div className={style.pageContainer}>
+        <style
+          dangerouslySetInnerHTML={{ __html: '.cesium-viewer-toolbar { right: 25vw }' }}
+        />
         <div className={style.cesium} id="cesiumContainer" ref={element => (this.cesiumContainer = element)} />
         <RightBar />
       </div>
     );
   }
-
 }
-
 
 export default CesiumPage;
