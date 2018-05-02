0a6af373b00126784d5a012cbc7e8433779d58e3
rladbsgh <therladbsgh@gmail.com>
2018-05-02 18:50:50 -0400

    Fix navigation css

--- client/modules/Desktop/Static/Navigation.js
+++ client/modules/Desktop/Static/Navigation.js
@@ -3,16 +3,23 @@ import { Link } from 'react-router';
 
 import style from './CesiumPage.css';
 
-import logo from '../../../../public/images/bse_blue.png';
+import logo from '../../../../public/images/bse_white.png';
 
 class Navigation extends Component {
   render() {
     return (
       <div className={style.navigation}>
-        <Link to="/" className={style.active}>Mission Control</Link>
-        <img className={style.logo} src={logo} height="40px" width="40px" alt="logo" />
-        <Link to="/cad">CAD</Link>
-        <Link to="/data">Data</Link>
+        <div className={style.logo}>
+          <img src={logo} width="35px" height="35px" alt="logo" />
+        </div>
+        <div className={style.links}>
+          <Link to="/" className={style.active}>Mission Control</Link>
+          <Link to="/cad">CAD</Link>
+          <Link to="/data">Data</Link>
+        </div>
+        <div className={style.social}>
+          social media here
+        </div>
       </div>
     );
   }
