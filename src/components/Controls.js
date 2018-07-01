538456d8aede230ed064330fbc3eda48c0f0cddc
rladbsgh <therladbsgh@gmail.com>
2018-06-30 21:14:30 -0400

    Add lock toggle

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -7,9 +7,16 @@ import '../assets/Controls.css';
 import SceneMode from "cesium/Source/Scene/SceneMode";
 
 class Controls extends Component {
+
   render() {
     return (
       <div className='controls'>
+      <button
+        onClick={this.props.toggleLocked}
+        className='button'
+        >
+          Toggle
+        </button>
         <button
         onClick={() => {
           this.props.mapLayer.alpha = 1;
@@ -28,16 +35,17 @@ class Controls extends Component {
         </button>
         <button
         onClick={() => {
-          const tlejs = new TLEJS();
-          const tleStr = 'ISS (ZARYA)\n1 25544U 98067A   18167.57342809  .00001873  00000-0  35452-4 0  9993\n2 25544  51.6416  21.7698 0002962 191.5103 260.7459 15.54186563118420';
-          const currentLoc = tlejs.getSatelliteInfo(tleStr, Date.now(), 0, 0, 0);
-          const position = Cartesian3.fromDegrees(currentLoc.lng,
-                                                  currentLoc.lat,
-                                                  currentLoc.height * 1000);
-
           this.props.viewer.scene.mode = SceneMode.SCENE2D;
-          this.props.viewer.camera.lookAt(position, new Cartesian3(0.0, 0.0, 15000000.0));
-          this.props.viewer.trackedEntity = this.props.entity;
+          if (this.props.locked) {
+            const tlejs = new TLEJS();
+            const tleStr = 'ISS (ZARYA)\n1 25544U 98067A   18167.57342809  .00001873  00000-0  35452-4 0  9993\n2 25544  51.6416  21.7698 0002962 191.5103 260.7459 15.54186563118420';
+            const currentLoc = tlejs.getSatelliteInfo(tleStr, Date.now(), 0, 0, 0);
+            const position = Cartesian3.fromDegrees(currentLoc.lng,
+                                                    currentLoc.lat,
+                                                    currentLoc.height * 1000);
+            this.props.viewer.camera.lookAt(position, new Cartesian3(0.0, 0.0, 15000000.0));
+            this.props.viewer.trackedEntity = this.props.entity;
+          }
         }}
         className='button'
         >
@@ -46,7 +54,9 @@ class Controls extends Component {
         <button
           onClick={() => {
             this.props.viewer.scene.mode = SceneMode.SCENE3D;
-            this.props.viewer.trackedEntity = this.props.entity;
+            if (this.props.locked) {
+              this.props.viewer.trackedEntity = this.props.entity;
+            }
           }}
           className='button'
         >
--- src/pages/Cesium.js
+++ src/pages/Cesium.js
@@ -29,7 +29,31 @@ class CesiumPage extends Component {
       latitude: 0,
       altitude: 0,
       velocity: 0,
+      locked: true,
     };
+
+    this.toggleLocked = this.toggleLocked.bind(this);
+  }
+
+  toggleLocked() {
+    if (this.state.locked) {
+      this.state.viewer.trackedEntity = null;
+      this.setState({
+        locked: false,
+      });
+    } else {
+      const tlejs = new TLEJS();
+      const tleStr = 'ISS (ZARYA)\n1 25544U 98067A   18167.57342809  .00001873  00000-0  35452-4 0  9993\n2 25544  51.6416  21.7698 0002962 191.5103 260.7459 15.54186563118420';
+      const currentLoc = tlejs.getSatelliteInfo(tleStr, Date.now(), 0, 0, 0);
+      const position = Cartesian3.fromDegrees(currentLoc.lng,
+                                              currentLoc.lat,
+                                              currentLoc.height * 1000);
+      this.state.viewer.camera.lookAt(position, new Cartesian3(0.0, 0.0, 15000000.0));
+      this.state.viewer.trackedEntity = this.state.entity;
+      this.setState({
+        locked: true,
+      });
+    }
   }
 
   componentDidMount() {
@@ -134,7 +158,9 @@ class CesiumPage extends Component {
                                                currentLoc.height * 1000);
       pathPosition.addSample(JulianDate.now(), position);
       entity.position = position;
-      viewer.trackedEntity = entity;
+      if (this.state.locked) {
+        viewer.trackedEntity = entity;
+      }
 
       this.setState({
         longitude: currentLoc.lng,
@@ -155,6 +181,8 @@ class CesiumPage extends Component {
               viewer={this.state.viewer}
               entity={this.state.entity}
               mapLayer={this.state.mapLayer}
+              toggleLocked={this.toggleLocked}
+              locked={this.state.locked}
             />
           }
           <Navigation active='missioncontrol' />
