db20e75fa328ee2f84d1d93354d5c5c524c39ebb
Tyler Fox <tyler_fox@brown.edu>
2018-07-17 16:44:49 -0400

    tle updates

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -19,7 +19,18 @@ class Controls extends Component {
 
     this.onChangeDimension = this.onChangeDimension.bind(this);
     this.onChangeMapLayer = this.onChangeMapLayer.bind(this);
+
+    fetch('http://tracking.brownspace.org/api/equisat_tle').then((res) => {      
+      if (res.status === 200) {        
+        res.text().then((res) => {
+          this.tleStr = res;
+        });
+      } else {
+        this.tleStr = 'ISS (ZARYA)\n1 25544U 98067A   18167.57342809  .00001873  00000-0  35452-4 0  9993\n2 25544  51.6416  21.7698 0002962 191.5103 260.7459 15.54186563118420';
+      }
+    });
   }
+  
 
   onChangeDimension(value) {
     if (value) {
@@ -30,9 +41,8 @@ class Controls extends Component {
     } else {
       this.props.viewer.scene.mode = SceneMode.SCENE2D;
       if (this.props.locked) {
-        const tlejs = new TLEJS();
-        const tleStr = 'ISS (ZARYA)\n1 25544U 98067A   18167.57342809  .00001873  00000-0  35452-4 0  9993\n2 25544  51.6416  21.7698 0002962 191.5103 260.7459 15.54186563118420';
-        const currentLoc = tlejs.getSatelliteInfo(tleStr, Date.now(), 0, 0, 0);
+        const tlejs = new TLEJS();        
+        const currentLoc = tlejs.getSatelliteInfo(this.tleStr, Date.now(), 0, 0, 0);
         const position = Cartesian3.fromDegrees(currentLoc.lng,
                                                 currentLoc.lat,
                                                 currentLoc.height * 1000);
